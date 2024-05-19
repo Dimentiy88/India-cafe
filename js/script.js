9e5fcc7cbfdfd64ef9b0a11598118a1cfc369f61
Dimentiy88 <bodartinov88@gmail.com>
2024-05-19 17:09:04 +0200

    scroll-up

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// ! -=-=-=-=-=-=-= Rewievs Arrows =-=-=-=-=-=-=-
+
 const items = document.querySelectorAll(".reviews__item");
 const prevBtn = document.querySelector(".prev-btn");
 const nextBtn = document.querySelector(".next-btn");
@@ -138,6 +140,33 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
-  handleMediaQueryChange(mediaQuery); // Initial check
-  mediaQuery.addListener(handleMediaQueryChange); // Listen for changes
+  handleMediaQueryChange(mediaQuery);
+  mediaQuery.addListener(handleMediaQueryChange);
 });
+
+// !=-=-=-=-=-=-= Scroll Button =-=-=-=-=-=--=-=
+
+let upButton = document.querySelector(".up-button");
+
+function scrollToTop() {
+  const scrollStep = -window.scrollY / (1000 / 16); // 1000 миллисекунд (1 секунда)
+  const scrollInterval = setInterval(function () {
+    if (window.scrollY !== 0) {
+      window.scrollBy(0, scrollStep);
+    } else {
+      clearInterval(scrollInterval);
+    }
+  }, 16); // 60 кадров в секунду
+}
+
+window.onscroll = function () {
+  if (window.scrollY > 200) {
+    upButton.classList.add("shown");
+  } else {
+    upButton.classList.remove("shown");
+  }
+};
+
+upButton.onclick = function () {
+  scrollToTop();
+};
