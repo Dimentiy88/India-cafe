09ed1f2ccdeb29e4c64313f28808dbb8faf851c5
Dimentiy88 <bodartinov88@gmail.com>
2024-06-09 18:08:40 +0200

    end

--- js/script.js
+++ js/script.js
@@ -143,30 +143,3 @@ document.addEventListener("DOMContentLoaded", function () {
   handleMediaQueryChange(mediaQuery);
   mediaQuery.addListener(handleMediaQueryChange);
 });
-
-// !=-=-=-=-=-=-= Scroll Button =-=-=-=-=-=--=-=
-
-let upButton = document.querySelector(".up-button");
-
-function scrollToTop() {
-  const scrollStep = -window.scrollY / (1000 / 16); // 1000 миллисекунд (1 секунда)
-  const scrollInterval = setInterval(function () {
-    if (window.scrollY !== 0) {
-      window.scrollBy(0, scrollStep);
-    } else {
-      clearInterval(scrollInterval);
-    }
-  }, 16); // 60 кадров в секунду
-}
-
-window.onscroll = function () {
-  if (window.scrollY > 200) {
-    upButton.classList.add("shown");
-  } else {
-    upButton.classList.remove("shown");
-  }
-};
-
-upButton.onclick = function () {
-  scrollToTop();
-};
